e3b1076c4887f639fc9b2d363973ba08aa916fc9
ChaosAttractor <lostattractor@gmail.com>
2025-08-17 20:09:00 +0800

    desktop/gnome: changing to gcr and wayland

--- platform/desktop/gnome/default.nix
+++ platform/desktop/gnome/default.nix
@@ -1,7 +1,9 @@
 _: {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Disable gcr-ssh-agent
+  services.gnome.gcr-ssh-agent.enable = false;
 
   imports = [ ./modules.nix ];
 }
