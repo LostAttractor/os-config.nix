e15659a1d1fdbb164544319c49f95216a5a446c1
ChaosAttractor <lostattractor@gmail.com>
2025-12-17 01:12:32 +0800

    gnome: GSK_RENDERER already use vulkan by default

--- user/desktop/default.nix
+++ user/desktop/default.nix
@@ -1,7 +1,4 @@
 _: {
   # Enable Wayland for Chromium (CEF) Apps
   home.sessionVariables.NIXOS_OZONE_WL = "1";
-  # Using vulkan renderer for gtk4
-  # home.sessionVariables.GSK_RENDERER = "vulkan";
-
 }
