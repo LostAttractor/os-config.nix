bf166dacf76561fc6b189981ab30a06c1d1c578c
ChaosAttractor <lostattractor@gmail.com>
2025-12-17 01:10:31 +0800

    obs-studio: use distroav

--- user/programs/obs-studio.nix
+++ user/programs/obs-studio.nix
@@ -4,12 +4,17 @@
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
       obs-pipewire-audio-capture
-      obs-ndi
+      distroav
       obs-text-pthread
       obs-vkcapture
       waveform
+      input-overlay
       # Plugins that no longer in use
       # obs-multi-rtmp
     ];
   };
+
+  home.packages = with pkgs; [
+    ndi-6
+  ];
 }
