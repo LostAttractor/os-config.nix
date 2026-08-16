e028d0328323e20ef3ac4939a44f2c4ef4ccab58
ChaosAttractor <lostattractor@gmail.com>
2026-08-17 06:43:15 +0800

    pulseaudio: disable PipeWire compatibility services

--- platform/modules/features/audio/pulseaudio.nix
+++ platform/modules/features/audio/pulseaudio.nix
@@ -3,4 +3,7 @@ _: {
   security.rtkit.enable = true;
   # PulseAudio
   hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+  services.pipewire.audio.enable = false;
+  services.pipewire.pulse.enable = false;
 }
