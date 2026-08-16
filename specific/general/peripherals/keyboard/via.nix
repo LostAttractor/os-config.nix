5ccc25147e1e571c1431a5fd97dd1363c7789142
ChaosAttractor <lostattractor@gmail.com>
2026-08-17 06:43:48 +0800

    via: package hidraw udev rules

--- specific/general/peripherals/keyboard/via.nix
+++ specific/general/peripherals/keyboard/via.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }: {
-  services.udev.extraRules = ''
-    KERNEL=="hidraw*", MODE="0660", GROUP="plugdev", TAG+="uaccess", TAG+="udev-acl"
-  '';
+  services.udev.packages = [ (pkgs.writeTextFile {
+    name = "hidraw-udev-rules";
+    destination = "/lib/udev/rules.d/60-hidraw.rules";
+    text = ''
+      # ACTION!="remove", SUBSYSTEM=="hidraw", TAG+="uaccess"
+      ACTION!="remove", SUBSYSTEM=="hidraw", GROUP="input", MODE="0660"
+    '';
+  }) ];
 
   environment.systemPackages = with pkgs; [ via ];
 }
