24f83ef0d318df328bc5cc7e48b32783ae2c73df
ChaosAttractor <lostattractor@gmail.com>
2026-08-17 06:41:11 +0800

    configuration: expose flake inputs argument

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,12 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   imports = [
