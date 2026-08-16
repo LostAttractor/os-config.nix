a8103a5677d6a97eaf7a7ed1cdeadb637c9cd339
ChaosAttractor <lostattractor@gmail.com>
2026-08-17 06:46:24 +0800

    gnome: trim redundant dconf settings

--- user/desktop/gnome/dconf/gnome.nix
+++ user/desktop/gnome/dconf/gnome.nix
@@ -1,19 +1,7 @@
 _: {
   dconf.settings = {
-    "org/gnome/mutter" = {
-      dynamic-workspaces = true;
-      edge-tiling = true;
-      workspaces-only-on-primary = true;
-    };
-    "org/gnome/desktop/peripherals/touchpad" = {
-      tap-to-click = true;
-    };
     "org/gnome/desktop/wm/preferences" = {
-      button-layout = "appmenu:minimize,maximize,close";
       resize-with-right-button = true;
-    };
-    "org/gnome/settings-daemon/plugins/power" = {
-      sleep-inactive-ac-type = "nothing";
     };
     "org/gnome/desktop/interface" = {
       clock-show-weekday = true;
