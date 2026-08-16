57e9886dd78ba3b28b19436dabece59f22ad76a5
ChaosAttractor <lostattractor@gmail.com>
2026-08-17 06:43:41 +0800

    xpadneo: package upstream udev rules

--- platform/modules/hardware/gamepad.nix
+++ platform/modules/hardware/gamepad.nix
@@ -1,15 +1,23 @@
-_: {
+{ pkgs, config, ... }:
+let
+  xpadneo-udev-rules = pkgs.stdenvNoCC.mkDerivation {
+    name = "xpadneo-udev-rules";
+    version = config.boot.kernelPackages.xpadneo.version;
+    src = config.boot.kernelPackages.xpadneo.src;
+    dontBuild = true;
+    installPhase = ''
+      runHook preInstall
+      install -d $out/lib/udev/rules.d
+      install -D hid-xpadneo/etc-udev-rules.d/*.rules $out/lib/udev/rules.d
+      runHook postInstall
+    '';
+  };
+in 
+{
   hardware = {
     xone.enable = true;
     xpadneo.enable = true;
   };
 
-  services.udev.extraRules = ''
-    ACTION=="add", KERNEL=="0005:045E:02FD.*|0005:045E:02E0.*|0005:045E:0B05.*|0005:045E:0B13.*|0005:045E:0B20.*|0005:045E:0B22.*", SUBSYSTEM=="hid", DRIVER!="xpadneo", ATTR{driver/unbind}="%k", ATTR{[drivers/hid:xpadneo]bind}="%k"
-    ACTION=="add|change", DRIVERS=="xpadneo", SUBSYSTEM=="input", ENV{ID_INPUT_JOYSTICK}=="1", TAG+="uaccess", MODE="0664", ENV{LIBINPUT_IGNORE_DEVICE}="1"
-
-    #FIXME(issue-291) Work around Steamlink not properly detecting the mappings
-    #FIXME(issue-457) Word around QMK overriding our hidraw rules
-    ACTION=="add|change", DRIVERS=="xpadneo", SUBSYSTEM=="hidraw", MODE:="0000", TAG-="uaccess"
-  '';
+  services.udev.packages = [ xpadneo-udev-rules ];
 }
