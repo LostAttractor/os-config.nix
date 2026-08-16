e2359f35e7aee8531fa886a6d3e8254e41d278a4
ChaosAttractor <lostattractor@gmail.com>
2026-08-17 06:54:32 +0800

    deploy: sync the flake before rebuilding

--- scripts/deploy-flake.sh
+++ scripts/deploy-flake.sh
@@ -3,4 +3,5 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-nixos-rebuild switch --flake .# $* --log-format internal-json -v |& nom --json
+rsync -rt --delete . /etc/nixos/
+nixos-rebuild $1 ${@:2} --log-format internal-json -v |& nom --json
