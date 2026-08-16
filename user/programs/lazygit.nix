9d90135c4a87b23452dc4e33ad0d9e70ce2f1702
ChaosAttractor <lostattractor@gmail.com>
2026-08-17 06:52:07 +0800

    lazygit: migrate difftastic renderer settings

--- user/programs/lazygit.nix
+++ user/programs/lazygit.nix
@@ -1,13 +1,16 @@
 _: {
   programs.lazygit = {
     enable = true;
-    settings.git = {
-      overrideGpg = true;
-      pagers = [
-        {
-          externalDiffCommand = "difft";
-        }
-      ];
+    settings = {
+      git = {
+        overrideGpg = true;
+        diffRenderers = [
+          {
+            command = "difft";
+            type = "extDiff";
+          }
+        ];
+      };
     };
   };
 }
