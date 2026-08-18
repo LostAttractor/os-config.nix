2c18f7b21eaed7473e0463676c59cde2d4d5d6a6
ChaosAttractor <lostattractor@gmail.com>
2026-08-18 17:25:53 +0800

    spicetify: enable only on supported Linux systems

--- user/programs/spicetify.nix
+++ user/programs/spicetify.nix
@@ -1,35 +1,40 @@
 {
   pkgs,
+  lib,
   inputs,
   config,
   ...
-}: {
+}:
+{
   # import the flake's module for your system
   imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
   # configure spicetify :)
-  programs.spicetify = 
-  let
-    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-  in {
-    enabledExtensions = with spicePkgs.extensions; [
-      adblock
-      hidePodcasts
-      shuffle # shuffle+ (special characters are sanitized out of extension names)
-    ];
+  config = lib.mkIf pkgs.stdenv.hostPlatform.isx86_64 {
+    programs.spicetify =
+      let
+        spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+      in
+      {
+        enabledExtensions = with spicePkgs.extensions; [
+          adblock
+          hidePodcasts
+          shuffle # shuffle+ (special characters are sanitized out of extension names)
+        ];
 
-    theme = spicePkgs.themes.sleek;
-    colorScheme = "Cherry";
+        theme = spicePkgs.themes.sleek;
+        colorScheme = "Cherry";
+      };
+
+    home.packages = [
+      (config.programs.spicetify.spicedSpotify.overrideAttrs (oldAttrs: {
+        postInstall =
+          oldAttrs.postInstall or ""
+          + ''
+            wrapProgram $out/bin/${oldAttrs.meta.mainProgram} \
+              --add-flags "--enable-wayland-ime"
+          '';
+      }))
+    ] ++ config.programs.spicetify.theme.extraPkgs;
   };
-
-  home.packages = [
-    (config.programs.spicetify.spicedSpotify.overrideAttrs (oldAttrs: {
-      postInstall =
-        oldAttrs.postInstall or ""
-        + ''
-          wrapProgram $out/bin/${oldAttrs.meta.mainProgram} \
-            --add-flags "--enable-wayland-ime"
-        '';
-    }))
-  ] ++ config.programs.spicetify.theme.extraPkgs;
 }
